332396ce0cba5760cbc958ae2f31531897e69e34
MagnusAOlsen <olsenmagnusa@gmail.com>
2025-12-02 23:11:43 +0100

    finished day2

---
+++ day2/part1.py
@@ -0,0 +1,10 @@
+with open("input.txt", "r") as f:
+    totalSum = 0
+    data = f.read().split(",")
+    for id_range in data:
+        endpoints = id_range.split("-")
+        secondary_range = [x for x in range(int(endpoints[0]), int(endpoints[1]) + 1)]
+        for number in secondary_range:
+            totalSum += number if str(number)[:int(len(str(number))/2)] == str(number)[int(len(str(number))/2):] and str(number)[0] != 0 else 0
+
+print(totalSum)
